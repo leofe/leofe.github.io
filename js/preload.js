ca4ed6867e2e563cbec587bfa3c414f6c1f91a0c
杨磊 <yanglei27@jd.com>
2018-08-09 18:34:16 +0800

    Site updated: 2018-08-09 18:34:16

--- js/preload.js
+++ js/preload.js
@@ -1,7 +1,6 @@
 
 (function(srcList) {
     for (var i = 0; i < srcList.length; i++) {
-        // var resource = srcList[i].replace(/http:|https:/, 'https:');
         var name = srcList[i];
         var arr = name.split('-');
         var resource = '//leofe.github.io/' + arr[0] + '/' + arr[1] + '/' + arr[2]+ '/' + name+'/';
@@ -12,5 +11,24 @@
         rsTag.setAttribute('href', resource);
         head.appendChild(rsTag);
     }
-})(['2018-01-01-book','2018-01-09-yanglei1','2018-01-09-yanglei2','2018-01-16-yanglei3','2018-01-17-yanglei4','2018-01-23-yanglei5','2018-01-24-yanglei6','2018-01-25-yanglei7','2018-02-05-yanglei8','2018-03-13-yanglei9','2018-04-04-yanglei10','2018-04-13-yanglei11','2018-04-13-yanglei12','2018-06-17-git','2018-07-13-pwa'])
+})([
+    '//leofe.github.io/2018/01/01/2018-01-01-book/',
+    '//leofe.github.io/2018/01/09/2018-01-09-yanglei1/',
+    '//leofe.github.io/2018/01/09/2018-01-09-yanglei2/',
+    '//leofe.github.io/2018/01/16/2018-01-16-yanglei3/',
+    '//leofe.github.io/2018/01/17/2018-01-17-yanglei4/',
+    '//leofe.github.io/2018/01/23/2018-01-23-yanglei5/',
+    '//leofe.github.io/2018/01/24/2018-01-24-yanglei6/',
+    '//leofe.github.io/2018/01/25/2018-01-25-yanglei7/',
+    '//leofe.github.io/2018/02/05/2018-02-05-yanglei8/',
+    '//leofe.github.io/2018/03/13/2018-03-13-yanglei9/',
+    '//leofe.github.io/2018/04/04/2018-04-04-yanglei10/',
+    '//leofe.github.io/2018/04/13/2018-04-13-yanglei11/',
+    '//leofe.github.io/2018/04/13/2018-04-13-yanglei12/',
+    '//leofe.github.io/2018/06/17/2018-06-17-git/',
+    '//leofe.github.io/2018/07/13/2018-07-13-pwa/',
+    '//leofe.github.io/about/',
+    '//leofe.github.io/comment/',
+    '//leofe.github.io/page/2/'
+])
         
