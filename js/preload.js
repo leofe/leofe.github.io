651218804cb61ba8dfe9fef6ff0f0387151a740e
杨磊 <yanglei27@jd.com>
2018-08-09 18:20:13 +0800

    Site updated: 2018-08-09 18:20:13

--- js/preload.js
+++ js/preload.js
@@ -1,3 +1,4 @@
+
 (function(srcList) {
     for (var i = 0; i < srcList.length; i++) {
         // var resource = srcList[i].replace(/http:|https:/, 'https:');
@@ -11,10 +12,5 @@
         rsTag.setAttribute('href', resource);
         head.appendChild(rsTag);
     }
-})([
-    '2018-01-01-book',
-    '2018-01-09-yanglei1',
-    '2018-01-16-yanglei3',
-    '2018-01-17-yanglei4',
-    '2018-07-13-pwa'
-])
+})(['2018-01-01-book','2018-01-09-yanglei1','2018-01-09-yanglei2','2018-01-16-yanglei3','2018-01-17-yanglei4','2018-01-23-yanglei5','2018-01-24-yanglei6','2018-01-25-yanglei7','2018-02-05-yanglei8','2018-03-13-yanglei9','2018-04-04-yanglei10','2018-04-13-yanglei11','2018-04-13-yanglei12','2018-06-17-git','2018-07-13-pwa'])
+        
