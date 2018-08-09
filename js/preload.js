e205f535309374d9a395f3013a3d4b1b73acb20c
杨磊 <yanglei27@jd.com>
2018-08-09 14:38:49 +0800

    Site updated: 2018-08-09 14:38:48

--- js/preload.js
+++ js/preload.js
@@ -1,12 +1,20 @@
 (function(srcList) {
     for (var i = 0; i < srcList.length; i++) {
-        var resource = srcList[i].replace(/http:|https:/, 'https:');
+        // var resource = srcList[i].replace(/http:|https:/, 'https:');
+        var name = srcList[i];
+        var arr = name.split('-');
+        var resource = 'https://leofe.github.io/' + arr[0] + '/' + arr[1] + '/' + arr[2]+ '/' + name;
         var head = document.getElementsByTagName('head')[0];
         var rsTag = document.createElement('link');
         rsTag.setAttribute('rel', 'prefetch');
+
         rsTag.setAttribute('href', resource);
         head.appendChild(rsTag);
     }
 })([
-    'https://leofe.github.io/2018/07/13/2018-07-13-pwa/'
+    '2018-01-01-book',
+    '2018-01-09-yanglei1',
+    '2018-01-16-yanglei3',
+    '2018-01-17-yanglei4',
+    '2018-07-13-pwa/'
 ])
